61a3f0eb0000f728a028d0150b0fd953df5ae986
RyzaT <ryantaylor50@hotmail.com>
2023-02-07 13:55:42 +0000

    start to add api

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const currentTempEl = document.getElementById('current-temp');
 
 const days = ['Sunday',  'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept','Oct', 'Nov', 'Dec'];
+
+const API_KEY = 'e58a91b5528ac74b5640933904b043fa';
 setInterval(() => {
     const time = new Date();
     const month = time.getMonth();
@@ -21,3 +23,17 @@ setInterval(() => {
     timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' + '<span id="am-pm">${ampm}</span>'
     dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
 },1000);
+
+
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        //console.log(success);
+
+        let {lat, lon } = sucess.coords;
+        fetch('https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}')
+        .then(res => res.json()).then(data => {
+            console.log(data)
+        })
+    })
+}
