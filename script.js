257ee80d4b3786a48d51c73f1f6dcdb9f0ce2918
RyzaT <ryantaylor50@hotmail.com>
2023-02-14 13:42:12 +0000

    slight adjustment to key

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const currentTempEl = document.getElementById('current-temp');
 const days = ['Sunday',  'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept','Oct', 'Nov', 'Dec'];
 
-const API_KEY = 'e58a91b5528ac74b5640933904b043fa';
+const API_KEY = '335d72c66a41f30289b3d859541ecfaa';
 setInterval(() => {
     const time = new Date();
     const month = time.getMonth();
@@ -20,7 +20,7 @@ setInterval(() => {
     const minute = time.getMinutes();
     const ampm = hour >=12 ? 'PM' :'AM'
 
-    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' + '<span id="am-pm">${ampm}</span>'
+    timeEl.innerHTML = hoursIn12HrFormat + ':' + minute + ' ' + '<span id="am-pm">${ampm}</span>'
     dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
 },1000);
 
@@ -30,10 +30,14 @@ function getWeatherData () {
     navigator.geolocation.getCurrentPosition((success) => {
         //console.log(success);
 
-        let {lat, lon } = sucess.coords;
-        fetch('https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}')
+        let {lat, lon } = success.coords;
+        fetch('https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}')
         .then(res => res.json()).then(data => {
             console.log(data)
+            showWeatherData(data);
         })
     })
+}
+function showWeatherData  (data){
+
 }
